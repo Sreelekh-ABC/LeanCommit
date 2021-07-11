d04b55d7a6bdef12ac3f00b991d10fa38531eed4
Sreelekh-ABC <sreelekh.nallatt@bank-abc.com>
2021-07-11 12:42:50 +0530

    Initial Commit

---
+++ LearnPurpose/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LearnPurpose
+//
+//  Created by sreelekh N on 11/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
